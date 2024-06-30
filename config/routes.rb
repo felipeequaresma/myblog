e5fa13d7b369b3a5361bfaa7a6d524ef0347f091
felipeequaresma <felipeequaresma@gmail.com>
2024-06-30 08:41:07 -0300

    fix: remove routes routes from namespace

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
+  mount_devise_token_auth_for 'User', at: 'auth', skip: %i[omniauth_callbacks registrations sessions passwords token_validations]
+
+  devise_scope :user do
+    post 'auth/login', to: 'devise_token_auth/sessions#create'
+    delete 'auth/logout', to: 'devise_token_auth/sessions#destroy'
+  end
+
   namespace :api, defaults: { format: 'json' } do
     namespace :v1 do
-      mount_devise_token_auth_for 'User', at: 'auth', skip: %i[omniauth_callbacks registrations sessions passwords token_validations]
-
-      devise_scope :user do
-        post 'auth/login', to: '/devise_token_auth/sessions#create'
-        delete 'auth/logout', to: '/devise_token_auth/sessions#destroy'
-      end
-
       resources :posts
       resources :users
     end
